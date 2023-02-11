8714300f4608a7f8f1e6e2b3dfb3c108402a0463
hanna <hannawkozak@gmail.com>
2023-02-11 22:27:02 +0000

    feat: add api server

--- api/server.js
+++ api/server.js
@@ -1,25 +1,28 @@
-const jsonServer = require("json-server");
+/* eslint-disable no-bitwise */
+/* eslint-disable consistent-return */
+/* eslint-disable import/no-extraneous-dependencies */
+const jsonServer = require('json-server');
 
 const db = {
   garage: [
     {
-      name: "Tesla",
-      color: "#e6e6fa",
+      name: 'Tesla',
+      color: '#e6e6fa',
       id: 1,
     },
     {
-      name: "BMW",
-      color: "#fede00",
+      name: 'BMW',
+      color: '#fede00',
       id: 2,
     },
     {
-      name: "Mersedes",
-      color: "#6c779f",
+      name: 'Mersedes',
+      color: '#6c779f',
       id: 3,
     },
     {
-      name: "Ford",
-      color: "#ef3c40",
+      name: 'Ford',
+      color: '#ef3c40',
       id: 4,
     },
   ],
@@ -42,39 +45,41 @@ const state = { velocity: {}, blocked: {} };
 
 server.use(middlewares);
 
-server.patch("/engine", (req, res) => {
+server.patch('/engine', (req, res) => {
   const { id, status } = req.query;
 
   if (!id || !status || !/^(started)|(stopped)|(drive)$/.test(status)) {
     return res
       .status(400)
       .send(
-        'Wrong parameters: "id" should be any positive number, "status" should be "started", "stopped" or "drive"'
+        'Wrong parameters: "id" should be any positive number, "status" should be "started", "stopped" or "drive"',
       );
   }
 
   if (!db.garage.find((car) => car.id === +id)) {
     return res
       .status(404)
-      .send("Car with such id was not found in the garage.");
+      .send('Car with such id was not found in the garage.');
   }
 
   const distance = 500000;
-  if (status === "drive") {
+  if (status === 'drive') {
     const velocity = state.velocity[id];
 
-    if (!velocity)
+    if (!velocity) {
       return res
         .status(404)
         .send(
-          'Engine parameters for car with such id was not found in the garage. Have you tried to set engine status to "started" before?'
+          'Engine parameters for car with such id was not found in the garage. Have you tried to set engine status to "started" before?',
         );
-    if (state.blocked[id])
+    }
+    if (state.blocked[id]) {
       return res
         .status(429)
         .send(
-          "Drive already in progress. You can't run drive for the same car twice while it's not stopped."
+          "Drive already in progress. You can't run drive for the same car twice while it's not stopped.",
         );
+    }
 
     state.blocked[id] = true;
 
@@ -85,7 +90,7 @@ server.patch("/engine", (req, res) => {
         delete state.velocity[id];
         delete state.blocked[id];
         res
-          .header("Content-Type", "application/json")
+          .header('Content-Type', 'application/json')
           .status(500)
           .send("Car has been stopped suddenly. It's engine was broken down.");
       }, (Math.random() * x) ^ 0);
@@ -94,7 +99,7 @@ server.patch("/engine", (req, res) => {
         delete state.velocity[id];
         delete state.blocked[id];
         res
-          .header("Content-Type", "application/json")
+          .header('Content-Type', 'application/json')
           .status(200)
           .send(JSON.stringify({ success: true }));
       }, x);
@@ -102,8 +107,7 @@ server.patch("/engine", (req, res) => {
   } else {
     const x = req.query.speed ? +req.query.speed : (Math.random() * 2000) ^ 0;
 
-    const velocity =
-      status === "started" ? Math.max(50, (Math.random() * 200) ^ 0) : 0;
+    const velocity = status === 'started' ? Math.max(50, (Math.random() * 200) ^ 0) : 0;
 
     if (velocity) {
       state.velocity[id] = velocity;
@@ -113,17 +117,16 @@ server.patch("/engine", (req, res) => {
     }
 
     setTimeout(
-      () =>
-        res
-          .header("Content-Type", "application/json")
-          .status(200)
-          .send(JSON.stringify({ velocity, distance })),
-      x
+      () => res
+        .header('Content-Type', 'application/json')
+        .status(200)
+        .send(JSON.stringify({ velocity, distance })),
+      x,
     );
   }
 });
 
 server.use(router);
 server.listen(PORT, () => {
-  console.log("Server is running on port", PORT);
+  console.log('Server is running on port', PORT);
 });
